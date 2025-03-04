5bd2e08cc3643cfaf00f8abb75637616475ba945
Pratik <prashar@microsoft.com>
2025-03-03 23:23:23 -0800

    updated setup file

--- setup.ps1
+++ setup.ps1
@@ -29,12 +29,12 @@ kubectl label namespace default istio.io/rev=asm-1-22
 # Deploy sample application
 kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.18/samples/bookinfo/platform/kube/bookinfo.yaml
 
-# Verfify services and pods
+# Verify services and pods
 kubectl get services
 kubectl get pods
 kubectl port-forward svc/productpage 12002:9080
 
-# Enable mTLS for the AKS cluster
+# Enable mTLS enforcement for default namespace in the cluster
 kubectl apply -n default -f - <<EOF
 apiVersion: security.istio.io/v1
 kind: PeerAuthentication
@@ -44,6 +44,9 @@ spec:
   mtls:
     mode: STRICT
 EOF
+
+# Verify your policy got deployed
+kubectl get peerauthentication -n default
 
 # Create azure monitor resource (managed prometheus resource)
 export AZURE_MONITOR_NAME="aksobserveworkspace"
@@ -61,6 +64,12 @@ az aks update --name $MY_AKS_CLUSTER_NAME --resource-group $MY_RESOURCE_GROUP_NA
 # Verify Azure monitor pods are running
 kubectl get pods -o wide -n kube-system | grep ama-
 
+# Create job and cofigmap for scraping istio metrics with prometheus
+kubectl create configmap ama-metrics-prometheus-config --from-file=prometheus-config -n kube-system
+
+# Get Istio version Installed for importing specific dashboards
+az aks show --resource-group $MY_RESOURCE_GROUP_NAME --name $MY_AKS_CLUSTER_NAME  --query 'serviceMeshProfile.istio.revisions'
+
 # Enable ACNS for the AKS cluster
 az aks update --resource-group $MY_RESOURCE_GROUP_NAME --name $MY_AKS_CLUSTER_NAME --enable-acns
 
@@ -68,69 +77,3 @@ az aks update --resource-group $MY_RESOURCE_GROUP_NAME --name $MY_AKS_CLUSTER_NA
 kubectl apply -f hubble-ui.yaml
 
 kubectl -n kube-system port-forward svc/hubble-ui 12000:80
-
-
-
-
-
-
-
-
-kubectl create configmap ama-metrics-prometheus-config --from-file=prometheus-config -n kube-system
-
-kubectl delete configmap ama-metrics-prometheus-config -n kube-system
-
-kubectl get pods -n kube-system
-
-kubectl port-forward -n kube-system svc/hubble-relay 4245:443
-
-kubectl get pods -o wide -n kube-system -l k8s-app=hubble-relay
-hubble observe --pod hubble-relay-76859646f7-2jb58
-hubble observe --protocol HTTP
-
-helm repo add kiali https://kiali.org/helm-charts
-
-helm repo update
-
-helm install \
-    --version=1.63.1 \
-    --set cr.create=true \
-    --set cr.namespace=aks-istio-system \
-    --namespace aks-istio-system \
-    --create-namespace \
-    kiali-operator \
-    kiali/kiali-operator
-
-# Generate a short-lived token to login to Kiali UI
-kubectl -n aks-istio-system create token kiali-service-account
-
-# Port forward to Istio service to access on http://localhost:20001
-kubectl port-forward svc/kiali 20001:20001 -n aks-istio-system
-
-kubectl run -it --rm aks-ssh --image=mcr.microsoft.com/aks/fundamental/base-ubuntu:v0.0.11
-
-kubectl exec --stdin --tty details-v1-6c98bc7c4c-5xsmx -- /bin/bash
-
-cat <<EOF | kubectl apply -f -
-apiVersion: v1
-kind: Pod
-metadata:
-  name: ubuntu
-  labels:
-    app: ubuntu
-spec:
-  containers:
-  - image: ubuntu
-    command:
-      - "sleep"
-      - "604800"
-    imagePullPolicy: IfNotPresent
-    name: ubuntu
-  restartPolicy: Always
-EOF
-
-kubectl exec -it ubuntu -- /bin/bash
-
-apt-get -y update; apt-get -y install curl
-
-curl http://10.244.1.106:9080 | grep -o "<title>.*</title>"
